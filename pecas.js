e3d06de170660f14c145964b2b4d56f8a0f619fb
Carlos Rangel <croi7726@gmail.com>
2022-06-21 19:38:35 -0300

    Contagen de pecas

---
+++ pecas.js
@@ -0,0 +1,21 @@
+let pesoPeca = 180
+let numeroPecas = 12
+let nomePeca = "PNEU" 
+
+if(pesoPeca >= 100) {
+    console.log("Pode-se cadastrar a peca")
+} else {
+    console.log("Peca com peso insuficiente")
+}
+
+if ((numeroPecas >= 10)) {
+    console.log("Impossivel cadastrar numero de pecas excedido")
+} else { 
+    console.log("Prossiga com o cadastro")
+}
+
+if (nomePeca.length < 3) {
+    console.log("Numero de caracteres insuficiente")
+} else {
+    console.log("Pode cadastrar")
+}
